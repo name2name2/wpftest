d62dd5485c15b1a6c59bbdaf909f26f72b9e0147
yang <Yang.Jian@grandsys.com>
2016-02-03 17:41:46 +0800

    0203

---
+++ WpfApplication1/OrdersCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Commands;
+
+namespace WpfApplication1
+{
+    public static class OrdersCommands
+    {
+        //TODO: 03 - The application defines a global SaveAll command which invokes the Save command on all registered Orders. It is enabled only when all orders can be saved.
+        public static CompositeCommand SaveAllOrdersCommand = new CompositeCommand();
+    }
+
+
+    /// <summary>
+    /// Provides a class wrapper around the static SaveAll command.
+    /// </summary>
+    public class OrdersCommandProxy
+    {
+        public virtual CompositeCommand SaveAllOrdersCommand
+        {
+            get { return OrdersCommands.SaveAllOrdersCommand; }
+        }
+    }
+}
