65c805f83e05036ca4461f852517f3b0b53e264b
yang <Yang.Jian@grandsys.com>
2016-02-04 17:34:31 +0800

    0204

---
+++ WpfApplication1/OrderModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+
+namespace WpfApplication1
+{
+    public class OrderModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+        private readonly IUnityContainer container;
+
+        public OrderModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            this.container = container;
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            this.container.RegisterType<IOrders>();
+        }
+
+    }
+}
---
+++ WpfApplication1/Services(D)/OrdersRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Services
+{
+    public class OrdersRepository : IOrdersRepository
+    {
+        private const int InitialOrdersCount = 3;
+        public IEnumerable<Order> GetOrdersToEdit()
+        {
+            var orders = new List<Order>(InitialOrdersCount);
+            for (int i = 1; i <= InitialOrdersCount; i++)
+            {
+                string orderName = String.Format(CultureInfo.CurrentCulture, "Order {0}", i);
+                orders.Add(new Order { Name = orderName, DeliveryDate = DateTime.Today.AddDays(i) });
+            }
+
+            return orders;
+        }
+    }
+}
